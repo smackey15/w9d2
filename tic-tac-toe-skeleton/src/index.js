079248e598bae8c1ff81786ed44b77b21d5283d8
YWBK <ywbkdev@gmail.com>
2022-02-01 14:42:09 -0500

    added require files

--- tic-tac-toe-skeleton/src/index.js
+++ tic-tac-toe-skeleton/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js") // the dot denotes we start in the same folder that index.js is in
+const Game = require("/ttt_node/game.js")
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
 });
+
+new Game() {}
---
+++ tic-tac-toe-skeleton/webpack_setup_steps.md
@@ -0,0 +1,6 @@
+1. create a .gitignore in the main repo
+2. added node_modules/ to the .gitignore
+3. inside the project directory, entered 'npm install webpack webpack-cli'
+4. then do npm init -y
+5. in package.json, add to scripts => "start": "webpack --watch --mode=development" 
+6. in terminal, npm start
